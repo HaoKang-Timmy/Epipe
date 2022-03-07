def57a6a41ebbf3f011d038f850c6385ed0608db
Beta Cat <466904389@qq.com>
2022-03-07 19:38:08 +0800

    Adding code

---
+++ dist_gpipe/__init__.py
@@ -0,0 +1,8 @@
+'''
+Author: kh
+Date: 2022-03-07 19:36:33
+LastEditTime: 2022-03-07 19:36:33
+
+'''
+from dist_gpipe.dist_gpipe import dist_gpipe
+from .distributedlayers import Reshape1,Reshape2
---
+++ dist_gpipe/distributedlayers/__init__.py
@@ -0,0 +1,8 @@
+'''
+Author: kh
+Date: 2022-03-07 19:36:03
+LastEditTime: 2022-03-07 19:36:03
+
+'''
+from .defered_bn import *
+from .distributed_layers import *
