8f452042ad80c3b4f445cd9b8c9079d0d6aabb69
timmywanttolearn <hao.kang.sys@gmail.com>
2022-04-20 09:34:42 -0400

    refine code

--- dataparallel_simulate/compression_simulation/compression_analyse/rand_tests.py
+++ dataparallel_simulate/compression_simulation/compression_analyse/rand_tests.py
@@ -8,20 +8,19 @@ import torch.nn as nn
 import argparse
 
 parser = argparse.ArgumentParser(description="Compression method test")
-parser.add_argument("--tensorsize", default=10000000, type=int)
+parser.add_argument("--tensorsize", default=1000000, type=int)
 parser.add_argument("--split", default=2, type=int)
 parser.add_argument("--sortquant", default=6, type=int)
 parser.add_argument("--quant", default=8, type=int)
 parser.add_argument("--kmeans", default=8, type=int)
 args = parser.parse_args()
 metric = MeanAbsoluteRelativeError()
-metric.reset
 kmeans_layer = KMeansLayer(args.kmeans, 0)
 input = torch.rand([args.tensorsize]).to(0)
-
 s_output = SortQuantization.apply(input, 6, 2)
 k_output = kmeans_layer(input)
 q_output = FakeQuantize.apply(input, 8)
+metric.reset
 metric._update([input, q_output])
 print("uniform quant MARE loss:", metric.compute())
 metric.reset
