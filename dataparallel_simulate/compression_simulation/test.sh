4e063f73265acda2f47575cc49c7d34e2ef70187
timmywanttolearn <hao.kang.sys@gmail.com>
2022-04-21 12:53:46 -0400

    prepare for cuda stream

--- dataparallel_simulate/compression_simulation/test.sh
+++ dataparallel_simulate/compression_simulation/test.sh
@@ -1,4 +1,8 @@
-python3 dataparallel_test_cv.py --pca1 8 --pca2 2 --log-dir ./test_pca8_2.txt
-python3 dataparallel_test_cv.py --pca 32 --log-dir ./test_pca32.txt
-python3 dataparallel_test_cv.py --pca 16 --log-dir ./test_pca16.txt
-python3 dataparallel_test_cv.py --pca 64 --log-dir ./test_pca64.txt
+# python3 dataparallel_test_cv.py --pca1 8 --log-dir ./test_pca8_2.txt
+# python3 dataparallel_test_cv.py --pca 32 --log-dir ./test_pca32.txt
+# python3 dataparallel_test_cv.py --pca 16 --log-dir ./test_pca16.txt
+# python3 dataparallel_test_cv.py --pca 64 --log-dir ./test_pca64.txt
+python3 dataparallel_test_cv.py --pca1 12 --log ./cifar10_pca12.txt
+python3 dataparallel_test_cv.py --pca1 6 --log ./cifar10_pca6.txt
+python3 dataparallel_test_cv.py --pca1 14 --log ./cifar10_pca14.txt
+python3 dataparallel_test_cv.py --pca1 4 --log ./cifar10_pca4.txt
