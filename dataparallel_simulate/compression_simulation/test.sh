e2a35f578a4b69618421a988c04f722a9e9d6c2c
timmywanttolearn <hao.kang.sys@gmail.com>
2022-04-23 22:53:30 -0400

    reporduce sh code

--- dataparallel_simulate/compression_simulation/test.sh
+++ dataparallel_simulate/compression_simulation/test.sh
@@ -1,8 +1,12 @@
-# python3 dataparallel_test_cv.py --pca1 8 --log-dir ./test_pca8_2.txt
-# python3 dataparallel_test_cv.py --pca 32 --log-dir ./test_pca32.txt
-# python3 dataparallel_test_cv.py --pca 16 --log-dir ./test_pca16.txt
-# python3 dataparallel_test_cv.py --pca 64 --log-dir ./test_pca64.txt
 python3 dataparallel_test_cv.py --pca1 12 --log ./cifar10_pca12.txt
 python3 dataparallel_test_cv.py --pca1 6 --log ./cifar10_pca6.txt
 python3 dataparallel_test_cv.py --pca1 14 --log ./cifar10_pca14.txt
 python3 dataparallel_test_cv.py --pca1 4 --log ./cifar10_pca4.txt
+python3 dataparallel_test_cv.py --sortquant --quant 12 --split 4 --log ./cifar10_q12s4.txt
+python3 dataparallel_test_cv.py --sortquant --quant 9 --split 3 --log ./cifar10_q9s3.txt
+python3 dataparallel_test_cv.py --sortquant --quant 6 --split 2 --log ./cifar10_q6s2.txt
+python3 dataparallel_test_cv.py --sortquant --quant 3 --split 1 --log ./cifar10_q3s1.txt
+python3 dataparallel_test_nlp.py --sortquant --quant 12 --split 4 --log ./rte_q12s4.txt
+python3 dataparallel_test_nlp.py --sortquant --quant 9 --split 3 --log ./rte_q9s3.txt
+python3 dataparallel_test_nlp.py --sortquant --quant 6 --split 2 --log ./rte_q6s2.txt
+python3 dataparallel_test_nlp.py --sortquant --quant 3 --split 1 --log ./rte_q3s1.txt
---
+++ dataparallel_simulate/ngpu_vs_parallel_pipe/test.sh
@@ -0,0 +1,4 @@
+python3 CIFAR10_nGPU.py --worker 2 
+python3 CIFAR10_nGPU.py --worker 4
+python3 NLP_nGPU.py --worker 2 
+python3 NLP_nGPU.py --worker 4
