a00b0c27bd48242e289e0b78e4b6cebbd06dfd7f
Beta Cat <466904389@qq.com>
2022-05-13 01:56:18 +0800

    simulate powersvd

--- dataparallel_simulate/compression_simulation/test.py
+++ dataparallel_simulate/compression_simulation/test.py
@@ -1,31 +1,49 @@
-"""
+'''
 Author: Beta Cat 466904389@qq.com
-Date: 2022-05-11 00:35:33
+Date: 2022-05-12 00:35:04
 LastEditors: Beta Cat 466904389@qq.com
-LastEditTime: 2022-05-11 20:24:59
-FilePath: /research/test/powersgd_test.py
+LastEditTime: 2022-05-13 01:53:48
+FilePath: /research/gpipe_test/dataparallel_simulate/compression_simulation/test.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-"""
 
-from powersgd import PowerSGD, Config, optimizer_step
+'''
+# """
+# Author: Beta Cat 466904389@qq.com
+# Date: 2022-05-11 00:35:33
+# LastEditors: Beta Cat 466904389@qq.com
+# LastEditTime: 2022-05-11 20:24:59
+# FilePath: /research/test/powersgd_test.py
+# Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+# """
+
+# from powersgd import PowerSGD, Config, optimizer_step
+# import torch
+# from utils import *
+# import time
+
+# params = torch.rand([64, 32, 112, 112]).to(1).requires_grad_()
+# some = torch.rand([64, 32, 112, 112]).to(1)
+# # print(params)
+# start = time.time()
+
+# powersgd = PowerSGD(
+#     params,
+#     config=Config(
+#         rank=20,  # lower rank => more aggressive compression
+#         min_compression_rate=1,  # don't compress gradients with less compression
+#         num_iters_per_step=20,  #   # lower number => more aggressive compression
+#         start_compressing_after_num_steps=0,
+#     ),
+# )
+# print(time.time() - start)
+# output = PowerPCA.apply(params, powersgd)
+# output.backward(some)
+from utils import PowerSVD,PowerSVDLayer
 import torch
-from utils import *
 import time
-
-params = torch.rand([64, 32, 112, 112]).to(1).requires_grad_()
-some = torch.rand([64, 32, 112, 112]).to(1)
-# print(params)
+layer = PowerSVDLayer(10,[64,32,112,112],3)
 start = time.time()
-
-powersgd = PowerSGD(
-    params,
-    config=Config(
-        rank=20,  # lower rank => more aggressive compression
-        min_compression_rate=1,  # don't compress gradients with less compression
-        num_iters_per_step=20,  #   # lower number => more aggressive compression
-        start_compressing_after_num_steps=0,
-    ),
-)
-print(time.time() - start)
-output = PowerPCA.apply(params, powersgd)
-output.backward(some)
+input = torch.rand([64,32,112,112])
+output = layer(input)
+end = time.time()
+print(end - start)
--- dataparallel_simulate/compression_simulation/utils.py
+++ dataparallel_simulate/compression_simulation/utils.py
@@ -764,3 +764,32 @@ class ReshapeSVD(autograd.Function):
         output = torch.matmul(output[..., :, :], V[..., :, :])
         output = output.view(shape)
         return output, None
+class PowerSVD(autograd.Function):
+    @staticmethod
+    def forward(ctx,input,p_buffer,q_buffer,iter):
+        for i in range(iter):
+            p_buffer[0] = torch.linalg.qr(p_buffer[0]).Q
+            q_buffer[0] = input @ p_buffer[0]
+            q_buffer[0] = torch.linalg.qr(q_buffer[0]).Q
+            p_buffer[0] = input.permute((0,1,3,2)) @ q_buffer[0]
+        ctx.p_buffer, ctx.q_buffer = p_buffer, q_buffer
+        ctx.iter = iter
+        return q_buffer[0] @ p_buffer[0].permute((0,1,3,2))
+    @staticmethod
+    def backward(ctx,grad_output):
+        iter = ctx.iter
+        p_buffer, q_buffer = ctx.p_buffer, ctx.q_buffer
+        for i in range(iter):
+            p_buffer[0] = torch.linalg.qr(p_buffer[0]).Q
+            q_buffer[0] = grad_output @ p_buffer[0]
+            q_buffer[0] = torch.linalg.qr(q_buffer[0]).Q
+            p_buffer[0] = grad_output.permute((0,1,3,2)) @ q_buffer[0]
+        return q_buffer[0] @ p_buffer[0].permute((0,1,3,2))
+class PowerSVDLayer(nn.Module):
+    def __init__(self,rank,shape,iter) -> None:
+        super(PowerSVDLayer,self).__init__()
+        self.p_buffer = [torch.randn((int(shape[0]),int(shape[1]),int(shape[2]),rank))]
+        self.q_buffer = [torch.randn((int(shape[0]),int(shape[1]),rank,int(shape[3])))]
+        self.iter = iter
+    def forward(self,input):
+        return PowerSVD.apply(input,self.p_buffer,self.q_buffer,self.iter)
