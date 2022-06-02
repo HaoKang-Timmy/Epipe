1e12fa4dca6dcd8cc56e351a1e78c758d21c6c00
Hao Kang <haokang@x1.mit.edu>
2022-06-02 13:32:03 +0000

    conv insert

--- pipeline_client/dist_gpipe_client/dist_gpipe.py
+++ pipeline_client/dist_gpipe_client/dist_gpipe.py
@@ -1,11 +1,4 @@
-"""
-Author: your name
-Date: 2022-04-03 11:38:29
-LastEditTime: 2022-04-12 19:18:55
-LastEditors: Please set LastEditors
-Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-FilePath: /research/gpipe_test/dist_gpipe_gloo/dist_gpipe.py
-"""
+
 from .client_utils import client
 from .utils import make_dictions
 import torch
--- pipeline_client/models/mobilenet_v2_seperate.py
+++ pipeline_client/models/mobilenet_v2_seperate.py
@@ -21,11 +21,15 @@ def mobilenet_v2_seperate(args):
     feature = model.features[0].children()
     conv = next(feature)
     bn = next(feature)
-    # conv1 = nn.Conv2d(32,20,(4,4),(4,4))
-    # t_conv1 = nn.ConvTranspose2d(320,1280,(1,1),(1,1))
-    layer1 = [conv, bn]
-    layer2 = [nn.ReLU6(inplace=False), model.features[1:]]
-    layer3 = [Reshape1(), model.classifier]
+    if args.convinsert is True:
+        conv1 = nn.Conv2d(32,20,(4,4),(4,4))
+        t_conv1 = nn.ConvTranspose2d(320,1280,(1,1),(1,1))
+        layer1 = [conv, bn,conv1]
+        layer3 = [t_conv1,Reshape1(), model.classifier]
+    else:
+        layer1 = [conv, bn]
+        layer2 = [nn.ReLU6(inplace=False), model.features[1:]]
+        layer3 = [Reshape1(), model.classifier]
     layer1 = nn.Sequential(*layer1)
     layer2 = nn.Sequential(*layer2)
     layer3 = nn.Sequential(*layer3)
