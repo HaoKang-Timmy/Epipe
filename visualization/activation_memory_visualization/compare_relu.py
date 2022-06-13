553d9d9b9a51555a9e9afc35f458432445ef04d3
Beta Cat <466904389@qq.com>
2022-06-14 01:24:59 +0800

    plot figure

--- visualization/activation_memory_visualization/compare_relu.py
+++ visualization/activation_memory_visualization/compare_relu.py
@@ -26,7 +26,7 @@ transform_train = transforms.Compose(
     ]
 )
 trainset = torchvision.datasets.CIFAR10(
-    root="../../data", train=True, download=True, transform=transform_train
+    root="../../../data", train=True, download=True, transform=transform_train
 )
 image = trainset[0][0]
 image = image.view(1, 3, 224, 224)
@@ -44,6 +44,7 @@ from scipy import stats
 
 plt.subplot(2, 2, 1)
 
+<<<<<<< Updated upstream
 data = sns.histplot(output1, bins=100)
 plt.title("activation memory distribution(conv+bn+relu)")
 plt.xlabel("(a) activation memory value")
@@ -54,23 +55,49 @@ data = sns.histplot(output3, bins=100)
 plt.title("activation memory distribution(conv+bn)")
 plt.xlabel("(b) activation memory value")
 plt.ylabel("count of values")
+=======
+data = sns.histplot(output1,bins = 100)
+plt.title("Activation Memory Distribution(conv+bn+relu)")
+plt.xlabel("(a) Activation Memory Value")
+plt.ylabel("Count of Values")
+
+plt.subplot(2, 2, 2)
+data = sns.histplot(output3,bins = 100)
+plt.title("Activation Memory Distribution(conv+bn)")
+plt.xlabel("(b) Activation Memory Value")
+plt.ylabel("Count of Values")
+>>>>>>> Stashed changes
 
 
 plt.subplot(2, 2, 3)
 hist, bin_edge = np.histogram(output1)
+<<<<<<< Updated upstream
 cdf = np.cumsum(hist / sum(hist))
 plt.xlabel("(c) weight value")
 plt.ylabel("cummulative distribution")
 plt.title("activation memory CDF(conv+bn+relu)")
+=======
+cdf = np.cumsum(hist/sum(hist))
+plt.xlabel("(c) Activation Memory Value")
+plt.ylabel("Cummulative Distribution")
+plt.title("Activation Memory CDF(conv+bn+relu)")
+>>>>>>> Stashed changes
 # plt.xlim([output1.min(),output1.max()])
 plt.plot(bin_edge[1:], cdf, label="CDF")
 plt.legend()
 plt.subplot(2, 2, 4)
 hist, bin_edge = np.histogram(output3)
+<<<<<<< Updated upstream
 cdf = np.cumsum(hist / sum(hist))
 plt.xlabel("(d) weight value")
 plt.ylabel("cummulative distribution")
 plt.title("activation memory CDF(conv+bn)")
+=======
+cdf = np.cumsum(hist/sum(hist))
+plt.xlabel("(d) Activation Memory Value")
+plt.ylabel("Cummulative Distribution")
+plt.title("Activation Memory CDF(conv+bn)")
+>>>>>>> Stashed changes
 # plt.xlim([output3.min(),output3.max()])
 plt.plot(bin_edge[1:], cdf, label="CDF")
 # plt.hist(output2, bins=100, density=True)
